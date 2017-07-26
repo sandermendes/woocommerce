b06b1f0b635cd45bb6c23cbd614fef4418e742ad
Sander Mendes <sandermendes@gmail.com>
2017-07-26 10:59:24 -0300

    Changes In Information Tab Products.

--- templates/single-product/tabs/additional-information.php
+++ templates/single-product/tabs/additional-information.php
@@ -22,12 +22,17 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 global $product;
 
-$heading = esc_html( apply_filters( 'woocommerce_product_additional_information_heading', __( 'Additional information', 'woocommerce' ) ) );
+$heading = esc_html( apply_filters( 'woocommerce_product_additional_information_heading', __( 'Additional Information', 'woocommerce' ) ) );
 
 ?>
 
 <?php if ( $heading ) : ?>
-	<h2><?php echo $heading; ?></h2>
+    <div class="well">
+        <h2><?php echo $heading; ?></h2>
 <?php endif; ?>
 
 <?php do_action( 'woocommerce_product_additional_information', $product ); ?>
+
+<?php if ( $heading ) : ?>
+    </div>
+<?php endif; ?>
